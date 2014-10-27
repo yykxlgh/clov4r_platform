6eb240929d92c91ce5fc50bcf305aa4adb17acce
李广华 <yykxlgh@126.com>
2014-10-27 10:57:39 +0800

    ceshi

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,15 +5,13 @@ import play.mvc.*;
 
 import java.util.*;
 
-import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
         render();
+        System.out.println("hello");
     }
-	
-	
 	
 
 }
